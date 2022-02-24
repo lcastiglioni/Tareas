bc06260b8315850e9a1914551ecba7b83e2f8750
Lucas Castiglioni <castiglionidamian@gmail.com>
2022-02-24 19:56:27 -0300

    prueba de subida

---
+++ tarea1.go
@@ -0,0 +1,13 @@
+package tareas
+
+import "fmt"
+
+const enero = 1
+
+const febrero = 2
+const marzo = 3
+
+func meses(int, int, int) {
+
+	fmt.Scanln()
+}
---
+++ tarea2.go
@@ -0,0 +1,14 @@
+package tareas
+
+import "fmt"
+
+1 := "numero primo"
+
+2
+
+func tarea2() {
+	if condition {
+
+	}
+
+}
