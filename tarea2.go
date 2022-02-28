ab739627c605351583185230826bbc005c605ce7
Lucas Castiglioni <castiglionidamian@gmail.com>
2022-02-28 15:34:35 -0300

    Arrado el archivo Tarea2

--- tarea2.go
+++ tarea2.go
@@ -1,13 +1,19 @@
-package tareas
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 var NumeroPrimo = [6]int{2, 3, 5, 7, 11, 13}
 
 var NumeroCompuesto = [6]int{4, 6, 8, 9, 10, 12}
 
-func tarea2() {
-	if NumeroPrimo != NumeroCompuesto {
+func main() {
+
+	fmt.Println(os.Args)
+
+	if NumeroPrimo == NumeroCompuesto {
 		fmt.Println("Es un numero primo")
 	} else {
 		fmt.Println("Es un numero compuesto")
